c17dbeeea56ab238c824defc70bb92d0ebfef97d
viveksaradva <saradavivek@gmail.com>
2025-02-04 15:34:38 +0530

    Completed the basic structure of account.py

--- ATM Simulator/account.py
+++ ATM Simulator/account.py
@@ -1,17 +1,35 @@
-class Account:
-    def __init__(self, account_number, owner, balance=0.0):
+import re
+from abc import ABC, abstractmethod
+
+class Account(ABC):
+    def __init__(self, account_number: str, owner: str  , balance: float=0.0):
         self.account_number = account_number
         self.owner = owner
         self._balance = balance # Private attr for encapsulation
 
+    @abstractmethod
     def calculate_interest(self):
-        pass
+        """
+        Abstract method for interest calculation
+        """
+        return 0.0
 
-    def deposit():
-        pass
+    def deposit(self, amount):
+        if re.match(amount, r"^\d+$"):
+            self._balance += amount
+            print(f"Deposited {amount} in the account {self.account_number} successfully.")
+        else:
+            print("Enter valid amount(e.g., 5000, 10000)")
 
-    def withdraw():
-        pass
+    def withdraw(self, amount):
+        if re.match(amount, r"^\d+$"):
+            if amount <= self._balance:
+                self._balance -= amount
+                print(f"Withdrawed {amount} from the account {self.account_number} successfully.")
+            else:
+                print("Insufficient balance!")
+        else:
+            print("Enter valid amount(e.g., 5000, 10000)")
 
-    def get_balance():
-        pass
+    def get_balance(self):
+        return self._balance
